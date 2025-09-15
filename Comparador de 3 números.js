90755f1553403730a6df4585f9bc83bd86f248fe
fragosonic98 <fragosonic98@gmail.com>
2025-09-15 16:11:21 -0600

    Avances del comparador de 3 números

---
+++ Comparador de 3 números.js
@@ -0,0 +1,7 @@
+// 1. Pedir 3 números al usuario 
+let threeNumbers = prompt('Agrega 3 números enteros, separados por una coma (,)');
+
+// 2. Declaramos la función para dividir el prompt y el usuario pueda agregar 3 cifras separadas por ",".
+let separadorDeNumeros = threeNumbers.split(",");
+
+// 3. Función para comparar los números
