5da1ef34b5ea9a7587433896ae958a3d98f05767
fragosonic98 <fragosonic98@gmail.com>
2025-09-17 22:00:02 -0600

    Update Comparador de 3 números.js
    Added rest of the program

--- Comparador de 3 números.js
+++ Comparador de 3 números.js
@@ -1,7 +1,23 @@
 // 1. Pedir 3 números al usuario 
-let threeNumbers = prompt('Agrega 3 números enteros, separados por una coma (,)');
+let numbersInput = prompt("Agrega 3 números enteros, separados por una coma (,)");
 
 // 2. Declaramos la función para dividir el prompt y el usuario pueda agregar 3 cifras separadas por ",".
-let separadorDeNumeros = threeNumbers.split(",");
+let separadorDeNumeros = numbersInput.split(",");
 
-// 3. Función para comparar los números
+// 3. Convertir los números
+let threeNumbers = separadorDeNumeros.map(num => parseFloat(num.trim()));
+// 4. Función para comparar los números
+let [a,b,c] = threeNumbers;
+
+// Si los números son iguales: imprimir un mensaje por consola o por el DOM diciendo que los números son iguales.
+if(a==b && b==c){
+      console.log("Los tres números introducidos: ",a,b,c,"son iguales" );
+} else{
+      //Ordenar de mayor a menor
+      threeNumbers.sort((a,b) => (b - a));
+      console.log("Números ordenados de mayor a menor: " ,threeNumbers);
+
+      //Ordenar de menor a mayor
+      threeNumbers.sort((a,b) => (a - b))
+      console.log("Números ordenados de menor a mayor: " ,threeNumbers);
+}
